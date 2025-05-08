20a7e44c6995b470375e8bf57854b91f9e2305ef
evldmt <lvadim3110@gmail.com>
2025-05-08 20:38:56 +0900

    v1

---
+++ guess the word.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+import random
+from tkinter import messagebox as mb
+
+window = tk.Tk()
+window.title("Guess the word")
+window.geometry("400x400")
+words = ["cake", "make", "take"]
+letters = []
+guess = random.choice(words)
+attempts = 3
+
+def new():
+    global guess
+    global letters
+    global attempts
+    m = mb.askyesno("New Game", "Wanna start a new game?")
+    if m == True:
+        attempts = 3
+        lab_att["text"] = f"attempts left: {attempts}"
+        letters = []
+        guess = random.choice(words)
+        label["text"] = "GUESS AGAIN"
+    else:
+        window.destroy()
+
+
+def check():
+    global guess
+    global attempts
+    global letters
+
+    lab = ""
+    letter = entry.get()
+    letters.append(letter)
+    entry.delete(0, tk.END)
+
+    for char in guess:
+        if char in letters:
+            lab += char
+        else:
+            lab += "*"
+        label["text"] = lab
+    if letter not in guess:
+        attempts -= 1
+        lab_att["text"] = f"attempts left: {attempts}"
+
+    if lab == guess:
+        mb.showinfo("Correct!", "U won")
+        new()
+
+    if attempts == 0:
+        mb.showinfo("((", "You lost")
+        new()
+
+
+
+
+label = tk.Label(window, text="Guess the word")
+lab_att = tk.Label(window, text=f"attempts: {attempts}")
+lab_att.place(x = 0, y = 0)
+label.place(x=100, y=50)
+
+entry = tk.Entry(window)
+entry.place(x=100, y=100)
+
+
+check = tk.Button(window, text="Check", command=check)
+check.place(x=200, y=300)
+window.mainloop()
